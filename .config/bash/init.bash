e3fdd6f86557a817e253e3a93c44e00de4d8accf
Mor <11615676+Heymor@users.noreply.github.com>
2024-02-13 19:10:40 -0500

    Updated bash history controls

--- .config/bash/init.bash
+++ .config/bash/init.bash
@@ -7,4 +7,13 @@ if [ -z "$INIT_BASH_SH_" ]; then
     fi
 
     PS1='\h:\W \u\$ '
+
+    # change history defaults
+    # expand history size
+    export HISTFILESIZE=10000
+    export HISTSIZE=500
+    # dont save duplicates
+    export HISTCONTROL=ignoreboth:erasedups
+    # append to history
+    shopt -s histappend
 fi
